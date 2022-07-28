99e976d13ea925bee51d1f873a92e722ff295ef5
ACASS28 <aac0428@tamu.edu>
2022-07-28 16:05:28 -0500

    fetch works!!! yay

--- jason.js
+++ jason.js
@@ -1,9 +1,13 @@
-async function load() {
-    fetch("./dogs.json")
-        .then((response) => response.json())
-        .then(res => res.json())
-        .then((data) => console.log(data));
-   
+async function load(url) {
+    // fetch("./dogs.json")
+    //     .then((response) => response.json())
+    //     .then(res => res.json())
+    //     .then((data) => console.log(data));
+    
+    
+        // Storing response
+    const response = await fetch(url);
+    var data = await response.json();
     // alert(mydata[0].name);
     // alert(mydata[0].age);
     document.getElementById("dog").textContent = data[0].name;
